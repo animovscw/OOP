c0ce9d3be9b402d86f9ea0d17d1384b7bce8358c
animovscw <animovscw@users.noreply.github.com>
2024-09-09 10:19:49 +0000

    Deploying to gh-pages from @ animovscw/OOP@d06c33482118f655a1e909b9b34b762e69d218f2 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.anisimov","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.anisimov","l":"Heapsort"}]
