ec33ec9f699c0da6c941de06cee97da30ecf5dd5
Karlusha06 <k.anisimov2@g.nsu.ru>
2024-09-09 17:42:22 +0700

    new tests

--- Task_1_1_1/src/test/java/ru/nsu/anisimov/HeapsortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/anisimov/HeapsortTest.java
@@ -14,6 +14,14 @@ class HeapsortTest {
         int[] test2Array = new int[]{4, 10, 3, 5, 1};
         Heapsort.heapsort(test2Array);
         assertArrayEquals(new int[]{1, 3, 4, 5, 10}, test2Array);
+
+        int[] test3Array = new int[]{2, 3, 4, 5};
+        Heapsort.heapsort(test3Array);
+        assertArrayEquals(new int[]{2, 3, 4, 5}, test3Array);
+
+        int[] test4Array = new int[]{1, 1, 1};
+        Heapsort.heapsort(test4Array);
+        assertArrayEquals(new int[]{1, 1, 1}, test4Array);
     }
 
 }
