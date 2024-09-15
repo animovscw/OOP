05df4d68dfe650326ca1b857f207a2c393819242
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2024-09-15 17:17:32 +0700

    modified

--- Task_1_1_2/src/main/java/ru/nsu/anisimov/Deck.java
+++ Task_1_1_2/src/main/java/ru/nsu/anisimov/Deck.java
@@ -1,7 +1,7 @@
 package ru.nsu.anisimov;
 
-import java.util.Collections;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
     final ArrayList<Card> cards;
@@ -9,9 +9,9 @@ public class Deck {
     public Deck() {
         cards = new ArrayList<>();
         Rank[] ranks = new Rank[]{
-                Rank.TWO, Rank.THREE, Rank.FOUR, Rank.FIVE, Rank.SIX,
-                Rank.SEVEN, Rank.EIGHT, Rank.NINE, Rank.TEN, Rank.JACK,
-                Rank.QUEEN, Rank.KING, Rank.ACE
+            Rank.TWO, Rank.THREE, Rank.FOUR, Rank.FIVE, Rank.SIX,
+            Rank.SEVEN, Rank.EIGHT, Rank.NINE, Rank.TEN, Rank.JACK,
+            Rank.QUEEN, Rank.KING, Rank.ACE
         };
         Suit[] suits = new Suit[]{Suit.HEART, Suit.DIAMOND, Suit.CLUB, Suit.SPADE};
         for (Suit suit : suits) {
