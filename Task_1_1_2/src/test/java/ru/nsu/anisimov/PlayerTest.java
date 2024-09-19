b934f3c437702057f8976ad65e0560d97575e37a
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2024-09-19 20:46:46 +0700

    added a test

--- Task_1_1_2/src/test/java/ru/nsu/anisimov/PlayerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/anisimov/PlayerTest.java
@@ -1,7 +1,16 @@
 package ru.nsu.anisimov;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 class PlayerTest {
-
+    @Test
+    void getCardTest() {
+        Player player = new Player();
+        Assertions.assertTrue(player.isAllCardsOpen());
+        Assertions.assertTrue(player.hand.isEmpty());
+        Assertions.assertFalse(player.getSum() != 0);
+    }
 }
