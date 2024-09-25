b016dec5fca3e1d00cc8f81c44979f401031e9b0
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2024-09-25 11:53:38 +0700

    new branch

---
+++ Task_1_1_3/src/main/java/ru/nsu/anisimov/Main.java
@@ -0,0 +1,11 @@
+package ru.nsu.anisimov;
+
+public class Main {
+    public int expression(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
