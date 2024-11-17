20d5f335d05354522f7a3c7bf10ec1c70406b5b0
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2024-11-17 20:19:18 +0700

    Fix reviewdog

--- Task_1_3_1/src/main/java/ru/nsu/anisimov/SubstringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/anisimov/SubstringSearch.java
@@ -3,9 +3,9 @@ package ru.nsu.anisimov;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.io.Reader;
 import java.util.Objects;
 
 /**
