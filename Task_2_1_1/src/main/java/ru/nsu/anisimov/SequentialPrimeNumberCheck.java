7ce64f424251d93636a0cb46a97c4b88ab4c39f8
animovscw <k.anisimov2@g.nsu.ru>
2025-02-10 20:01:43 +0700

    Sequential realization with tests

---
+++ Task_2_1_1/src/main/java/ru/nsu/anisimov/SequentialPrimeNumberCheck.java
@@ -0,0 +1,31 @@
+package ru.nsu.anisimov;
+
+public class PrimeNumberCheck {
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number == 2 || number == 3) {
+            return true;
+        }
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i <= Math.sqrt(number); i = i + 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean NonPrimeSequential(int[] array) {
+        for (int num : array) {
+            if (!isPrime(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Task_2_1_1/src/test/java/ru/nsu/anisimov/SequentialPrimeNumberCheckTest.java
@@ -0,0 +1,25 @@
+package ru.nsu.anisimov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PrimeNumberCheckTest {
+    @Test
+    void testNonPrimeSequential() {
+        int[] arr1 = {6, 8, 7, 13, 5, 9, 4};
+        int[] arr2 = {1, 6, 8, 7, 13, 5, 9, 4};
+        int[] arr3 = {49, 6, 8, 7, 13, 5, 9, 4};
+        Assertions.assertTrue(PrimeNumberCheck.NonPrimeSequential(arr1));
+        Assertions.assertTrue(PrimeNumberCheck.NonPrimeSequential(arr2));
+        Assertions.assertTrue(PrimeNumberCheck.NonPrimeSequential(arr3));
+    }
+
+    @Test
+    void testPrimeSequential() {
+        int[] arr1 = {20319251, 6997901, 6997927, 6997937, 17858849, 6997967, 6998009, 6998029, 6998039, 20165149, 6998051, 6998053};
+        int[] arr2 = {2, 20319251, 6997901, 6997927, 6997937, 17858849, 6997967, 6998009, 6998029, 6998039, 20165149, 6998051, 6998053};
+        Assertions.assertFalse(PrimeNumberCheck.NonPrimeSequential(arr1));
+        Assertions.assertFalse(PrimeNumberCheck.NonPrimeSequential(arr2));
+    }
+
+}
