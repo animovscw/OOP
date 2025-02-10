5a86a8aef83be89c2d0b1de2f0cd8b1715ba412f
animovscw <animovscw@users.noreply.github.com>
2025-02-10 13:22:49 +0000

    Deploying to gh-pages from @ animovscw/OOP@e316c540dea521226551108e29dc15c8350467eb 🚀

---
+++ Task_2_1_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.anisimov","c":"SequentialPrimeNumberCheck","l":"isPrime(int)"},{"p":"ru.nsu.anisimov","c":"SequentialPrimeNumberCheck","l":"NonPrimeSequential(int[])"},{"p":"ru.nsu.anisimov","c":"SequentialPrimeNumberCheck","l":"SequentialPrimeNumberCheck()","u":"%3Cinit%3E()"}];updateSearchResults();
