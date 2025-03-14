a7bd127e7d69c23ea0dcac256ce3d1bceb688737
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2025-03-14 13:06:42 +0700

    Added tests to check critical situations

--- Task_2_2_1/src/test/java/ru/nsu/anisimov/PizzeriaTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/anisimov/PizzeriaTest.java
@@ -27,4 +27,64 @@ class PizzeriaTest {
             Assertions.assertFalse(courier.isAlive());
         }
     }
+
+    @Test
+    void testOrderQueueHandling() throws InterruptedException {
+        Pizzeria pizzeria = new Pizzeria(1, new int[]{2}, 1, new int[]{1}, 2); // Склад вмещает 2 заказа
+        Order order1 = new Order(1);
+        Order order2 = new Order(2);
+        Order order3 = new Order(3);
+
+        pizzeria.addOrder(order1);
+        pizzeria.addOrder(order2);
+        pizzeria.addOrder(order3);
+
+        Thread.sleep(7000);
+
+        Assertions.assertTrue(pizzeria.getStorage().getStorageSize() <= 2);
+
+        pizzeria.shutdown();
+    }
+
+    @Test
+    void testStorageOverflowAndRelease() throws InterruptedException {
+        Pizzeria pizzeria = new Pizzeria(1, new int[]{2}, 1, new int[]{2}, 2); // Курьер забирает 2 заказа за раз
+        Order order1 = new Order(1);
+        Order order2 = new Order(2);
+        Order order3 = new Order(3);
+
+        pizzeria.addOrder(order1);
+        pizzeria.addOrder(order2);
+        pizzeria.addOrder(order3);
+
+        Thread.sleep(5000);
+
+        Assertions.assertEquals(1, pizzeria.getStorage().getStorageSize());
+
+        pizzeria.shutdown();
+    }
+
+    @Test
+    void testShutdownStopsAllThreads() throws InterruptedException {
+        Pizzeria pizzeria = new Pizzeria(2, new int[]{1, 1}, 2, new int[]{1, 1}, 3);
+        Order order1 = new Order(1);
+        Order order2 = new Order(2);
+
+        pizzeria.addOrder(order1);
+        pizzeria.addOrder(order2);
+
+        Thread.sleep(3000);
+
+        pizzeria.shutdown();
+
+        for (Thread baker : pizzeria.getBakers()) {
+            baker.join();
+            Assertions.assertFalse(baker.isAlive());
+        }
+
+        for (Thread courier : pizzeria.getCouriers()) {
+            courier.join();
+            Assertions.assertFalse(courier.isAlive());
+        }
+    }
 }
