2094e048ca6111fc55ada0a9f2c169187f11bb79
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2025-03-14 12:23:31 +0700

    Added new tests

--- Task_2_2_1/src/test/java/ru/nsu/anisimov/SimulatorTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/anisimov/SimulatorTest.java
@@ -6,37 +6,83 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimulatorTests {
-    private Pizzeria pizzeria;
+    private OrderQueue orderQueue;
+    private Storage storage;
 
     @BeforeEach
     void setUp() {
-        int[] bakerSpeeds = {2, 3, 1};
-        int[] courierCapacities = {2, 3};
-        int storageCapacity = 5;
-        pizzeria = new Pizzeria(bakerSpeeds.length, bakerSpeeds, courierCapacities.length, courierCapacities, storageCapacity);
+        orderQueue = new OrderQueue();
+        storage = new Storage(5);
     }
 
     @Test
-    void testOrderProcessing() throws InterruptedException {
+    void testOrderQueueFunctionality() throws InterruptedException {
         Order order = new Order(1);
-        pizzeria.addOrder(order);
-        assertNotNull(pizzeria.takeOrder());
+        orderQueue.addOrder(order);
+        assertEquals(order, orderQueue.takeOrder());
     }
 
     @Test
-    void testStorageCapacity() throws InterruptedException {
+    void testStorageCapacityLimit() throws InterruptedException {
         for (int i = 1; i <= 5; i++) {
-            pizzeria.storePizza(new Order(i));
+            storage.storeOrder(new Order(i));
         }
-        assertEquals(5, pizzeria.getStorageSize());
+        assertEquals(5, storage.getStorageSize());
+
+        Thread thread = new Thread(() -> {
+            try {
+                storage.storeOrder(new Order(6));
+            } catch (InterruptedException ignored) {
+            }
+        });
+        thread.start();
+
+        Thread.sleep(500);
+        assertEquals(5, storage.getStorageSize());
+
+        storage.takeOrder();
+        Thread.sleep(500);
+        assertEquals(5, storage.getStorageSize());
+    }
+
+    @Test
+    void testStorageOrderRetrieval() throws InterruptedException {
+        Order order1 = new Order(1);
+        Order order2 = new Order(2);
+
+        storage.storeOrder(order1);
+        storage.storeOrder(order2);
+
+        assertEquals(order1, storage.takeOrder());
+        assertEquals(order2, storage.takeOrder());
+    }
+
+    @Test
+    void testBakerProcessing() throws InterruptedException {
+        Baker baker = new Baker(1, 1, orderQueue, storage);
+        Thread bakerThread = new Thread(baker);
+
+        orderQueue.addOrder(new Order(1));
+        bakerThread.start();
+
+        Thread.sleep(1500);
+        assertEquals(1, storage.getStorageSize());
+
+        bakerThread.interrupt();
     }
 
     @Test
     void testCourierDelivery() throws InterruptedException {
-        for (int i = 1; i <= 3; i++) {
-            pizzeria.storePizza(new Order(i));
-        }
-        pizzeria.deliverPizza(1, 2);
-        assertTrue(pizzeria.getStorageSize() <= 1);
+        Courier courier = new Courier(1, 2, storage);
+        Thread courierThread = new Thread(courier);
+
+        storage.storeOrder(new Order(1));
+        storage.storeOrder(new Order(2));
+        courierThread.start();
+
+        Thread.sleep(3500);
+        assertEquals(0, storage.getStorageSize());
+
+        courierThread.interrupt();
     }
 }
