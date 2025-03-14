074832777854128e16e5f29454fd410d5d4312a2
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2025-03-14 19:38:14 +0700

    Fixed javadoc

--- Task_2_2_1/src/test/java/ru/nsu/anisimov/PizzeriaTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/anisimov/PizzeriaTest.java
@@ -1,7 +1,7 @@
 package ru.nsu.anisimov;
 
-import org.junit.jupiter.api.Assertions;
 import java.io.IOException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class PizzeriaTest {
