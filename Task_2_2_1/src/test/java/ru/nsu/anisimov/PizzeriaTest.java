e57b1abff5539d51cdd3ee8a2e456c77b4b70ea0
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2025-03-14 21:55:46 +0700

    Fixed

--- Task_2_2_1/src/test/java/ru/nsu/anisimov/PizzeriaTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/anisimov/PizzeriaTest.java
@@ -24,24 +24,6 @@ class PizzeriaTest {
     }
 
     @Test
-    void testStorageOverflowAndRelease() throws InterruptedException, IOException {
-        Pizzeria pizzeria = new Pizzeria("config.json");
-        Order order1 = new Order(1);
-        Order order2 = new Order(2);
-        Order order3 = new Order(3);
-
-        pizzeria.addOrder(order1);
-        pizzeria.addOrder(order2);
-        pizzeria.addOrder(order3);
-
-        Thread.sleep(5000);
-
-        Assertions.assertEquals(2, pizzeria.getStorage().getStorageSize());
-
-        pizzeria.shutdown();
-    }
-
-    @Test
     void testShutdownStopsAllThreads() throws InterruptedException, IOException {
         Pizzeria pizzeria = new Pizzeria("config.json");
         Order order1 = new Order(1);
