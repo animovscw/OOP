d1e10dc712d335fc20afcb4d3783877e8722b84b
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2025-03-14 12:21:31 +0700

    Updated method Courier

--- Task_2_2_1/src/main/java/ru/nsu/anisimov/Courier.java
+++ Task_2_2_1/src/main/java/ru/nsu/anisimov/Courier.java
@@ -3,22 +3,27 @@ package ru.nsu.anisimov;
 public class Courier implements Runnable {
     private final int id;
     private final int capacity;
-    private final Pizzeria pizzeria;
+    private final Storage storage;
 
-    public Courier(int id, int capacity, Pizzeria pizzeria) {
+    public Courier(int id, int capacity, Storage storage) {
         this.id = id;
         this.capacity = capacity;
-        this.pizzeria = pizzeria;
+        this.storage = storage;
     }
 
     @Override
     public void run() {
         try {
             while (!Thread.currentThread().isInterrupted()) {
-                pizzeria.deliverPizza(id, capacity);
+                for (int i = 0; i < capacity; ++i) {
+                    Order order = storage.takeOrder();
+                    System.out.println("[Курьер #" + id + "] Доставляет " + order);
+                    Thread.sleep(3000);
+                }
             }
 
         } catch (InterruptedException e) {
+            System.out.println("[Курьер #" + id + "] Завершает работу");
             Thread.currentThread().interrupt();
         }
     }
