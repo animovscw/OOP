7fef82191f92b561fa3396d057f0749be8cf4e3d
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2025-03-14 13:14:10 +0700

    Deleted failed test

--- Task_2_2_1/src/test/java/ru/nsu/anisimov/PizzeriaTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/anisimov/PizzeriaTest.java
@@ -4,30 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 class PizzeriaTest {
-
-    @Test
-    void testPizzeriaStartupAndShutdown() throws InterruptedException {
-        Pizzeria pizzeria = new Pizzeria(2, new int[]{2, 3}, 1, new int[]{2}, 5);
-        Order order1 = new Order(1);
-        Order order2 = new Order(2);
-
-        pizzeria.addOrder(order1);
-        pizzeria.addOrder(order2);
-
-        Thread.sleep(5000);
-
-        Assertions.assertTrue(pizzeria.getStorage().getStorageSize() > 0);
-
-        pizzeria.shutdown();
-        for (Thread baker : pizzeria.getBakers()) {
-            baker.join();
-            Assertions.assertFalse(baker.isAlive());
-        }
-        for (Thread courier : pizzeria.getCouriers()) {
-            Assertions.assertFalse(courier.isAlive());
-        }
-    }
-
     @Test
     void testOrderQueueHandling() throws InterruptedException {
         Pizzeria pizzeria = new Pizzeria(1, new int[]{2}, 1, new int[]{1}, 2); // Склад вмещает 2 заказа
