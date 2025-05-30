62c0d372abc7f441e8e80a32b57c0e40a71aedad
Kirill Anisimov <k.anisimov2@g.nsu.ru>
2025-05-30 23:55:05 +0700

    modified

--- Task_2_1_2/src/main/java/ru/nsu/anisimov/distributed/server/PrimeServer.java
+++ Task_2_1_2/src/main/java/ru/nsu/anisimov/distributed/server/PrimeServer.java
@@ -1,15 +1,28 @@
 package ru.nsu.anisimov.distributed.server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import ru.nsu.anisimov.distributed.common.Result;
 import ru.nsu.anisimov.distributed.common.Task;
 import ru.nsu.anisimov.distributed.worker.PrimeWorker;
-
-import java.io.*;
-import java.net.*;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Prime number checking server that distributes work across multiple worker nodes.
@@ -20,6 +33,11 @@ public class PrimeServer {
     public static final int WORKER_TIMEOUT_MS = 3000;
     private static final int DISCOVERY_PORT = 8888;
 
+    /**
+     * Waits for worker connections within specified timeout.
+     *
+     * @return list of connected worker sockets
+     */
     public static List<Socket> waitForWorkers() throws IOException {
         ServerSocket serverSocket = new ServerSocket(PORT);
         serverSocket.setSoTimeout(TIMEOUT_MS);
@@ -42,12 +60,22 @@ public class PrimeServer {
         return workers;
     }
 
+    /**
+     * Be ready.
+     *
+     * @return toto
+     */
     private static int[] prepareArray() {
         int size = 20;
         int maxVal = 1000000;
         return new Random().ints(size, 1, maxVal).toArray();
     }
 
+    /**
+     * Processes sub-task by sending it to worker and receiving result.
+     *
+     * @return true if subarray contains non-prime numbers
+     */
     public static boolean processSubTask(Socket worker, Task task) throws IOException, ClassNotFoundException {
         try (ObjectOutputStream out = new ObjectOutputStream(worker.getOutputStream());
              ObjectInputStream in = new ObjectInputStream(worker.getInputStream())) {
@@ -58,6 +86,13 @@ public class PrimeServer {
         }
     }
 
+    /**
+     * Distributes array checking across workers.
+     *
+     * @param array numbers to check
+     * @param workers available worker connections
+     * @return true if array contains non-prime numbers
+     */
     public static boolean processArray(int[] array, List<Socket> workers) throws InterruptedException {
         int n = array.length;
         int w = workers.size();
@@ -93,7 +128,7 @@ public class PrimeServer {
                 }
             } catch (ExecutionException e) {
                 System.err.println("Task execution error: " + e.getMessage());
-                hasNonPrime.set(true);  // assume failure means non-prime for safety
+                hasNonPrime.set(true);
             }
         }
 
@@ -109,6 +144,9 @@ public class PrimeServer {
         return hasNonPrime.get();
     }
 
+    /**
+     * Discovery.
+     */
     private static void broadcastDiscovery() {
         try (DatagramSocket ds = new DatagramSocket()) {
             ds.setBroadcast(true);
@@ -127,6 +165,10 @@ public class PrimeServer {
         }
     }
 
+    /**
+     * Main.
+     * @param args arg
+     */
     public static void main(String[] args) {
         try {
             broadcastDiscovery();
